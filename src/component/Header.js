a4ab1f823858c22f13f92d4c516f053fda86033a
asimazher <asimazher7oct@gmail.com>
2023-07-03 18:27:29 +0500

    Routing Implemented

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,15 +1,19 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
 
 const Header = ({ onShowAdd, onShow }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <h1>Task Tracker</h1>
-      <Button
-        color={onShow ? "red" : "lightseagreen"}
-        text={onShow ? "Close" : "Add"}
-        onClick={onShowAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={onShow ? "red" : "lightseagreen"}
+          text={onShow ? "Close" : "Add"}
+          onClick={onShowAdd}
+        />
+      )}
     </header>
   );
 };
